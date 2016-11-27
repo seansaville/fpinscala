e09b1ef92c8783d9e1d638371afbf7d4ced4dcc7
Sean Saville <sean.saville@hotmail.co.uk>
2016-11-27 02:16:02 +0000

    Added solution to Exercise 2.1

---
+++ src/main/scala/uk/co/seansavile/fpinscala/gettingstarted/Exercise1.scala
@@ -0,0 +1,19 @@
+package uk.co.seansavile.fpinscala.gettingstarted
+
+import scala.annotation.tailrec
+
+/**
+  * Exercise 2.1: Write a recursive function to get the nth Fibonacci number.
+  */
+object Exercise1 {
+
+  def fib(n: Int): Int = {
+    @tailrec
+    def fibAcc(n: Int, prev1: Int, prev2: Int): Int = {
+      if (n == 0) prev1
+      else fibAcc(n - 1, prev2, prev1 + prev2)
+    }
+    fibAcc(n, 0, 1)
+  }
+
+}

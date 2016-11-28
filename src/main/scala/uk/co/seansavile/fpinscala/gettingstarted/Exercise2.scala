513e1c955b2e01cf4d900fee209a9c30bfd0d69f
Sean Saville <sean.saville@hotmail.co.uk>
2016-11-28 23:40:49 +0000

    Added solution to Exercise 2.2

---
+++ src/main/scala/uk/co/seansavile/fpinscala/gettingstarted/Exercise2.scala
@@ -0,0 +1,21 @@
+package uk.co.seansavile.fpinscala.gettingstarted
+
+import scala.annotation.tailrec
+
+/**
+  * Exercise 2.2: Implement isSorted, which checks whether an Array[A] is sorted according to a
+  * given comparison function.
+  */
+object Exercise2 {
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @tailrec
+    def loop(n: Int): Boolean = {
+      if (n >= as.length - 1) true
+      else if (ordered(as(n), as(n + 1))) loop(n + 1)
+      else false
+    }
+    loop(0)
+  }
+
+}

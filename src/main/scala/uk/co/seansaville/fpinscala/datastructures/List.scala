85d91fcb4e11808ddf1daa1ab174eae4431f4c15
Sean Saville <sean.saville@hotmail.co.uk>
2016-11-30 20:30:36 +0000

    Added solution to Exercise 3.9

--- src/main/scala/uk/co/seansaville/fpinscala/datastructures/List.scala
+++ src/main/scala/uk/co/seansaville/fpinscala/datastructures/List.scala
@@ -59,4 +59,17 @@ object List {
     iterate(ls, Nil)
   }
 
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B =
+    as match {
+      case Nil         => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+
+  /**
+    * Exercise 3.9: Compute the length of a list using foldRight.
+    */
+  def length[A](ls: List[A]): Int = {
+    foldRight(ls, 0)((_, n) => n + 1)
+  }
+
 }

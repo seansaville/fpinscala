ef7602513e6961e0aaf1f35ae3867c42cce4dd75
Sean Saville <sean.saville@hotmail.co.uk>
2016-12-13 22:42:36 +0000

    Added solutions to remaining Option exercises (4.3 - 4.5)

--- src/main/scala/uk/co/seansaville/fpinscala/errorhandling/Option.scala
+++ src/main/scala/uk/co/seansaville/fpinscala/errorhandling/Option.scala
@@ -46,4 +46,31 @@ object Option {
   def variance(xs: Seq[Double]): Option[Double] =
     mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
 
+  /**
+    * Exercise 4.3: Implement map2, which combines two Option values using a binary function.
+    */
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a flatMap (a2 => b map (b2 => f(a2, b2)))
+
+  /**
+    * Exercise 4.4: Implement sequence, which combines a list of Options into one option.
+    */
+  def sequence[A](ls: List[Option[A]]): Option[List[A]] = ls match {
+    case Nil     => Some(Nil)
+    case a :: as => a flatMap (a2 => sequence(as) map (a2 :: _))
+  }
+
+  /**
+    * Exercise 4.5: Implement traverse.
+    */
+  def traverse[A, B](ls: List[A])(f: A => Option[B]): Option[List[B]] = ls match {
+    case Nil     => Some(Nil)
+    case a :: as => map2(f(a), traverse(as)(f))(_ :: _)
+  }
+
+  /**
+    * And then sequence in terms of traverse.
+    */
+  def sequenceByTraverse[A](ls: List[Option[A]]): Option[List[A]] = traverse(ls)(e => e)
+
 }
